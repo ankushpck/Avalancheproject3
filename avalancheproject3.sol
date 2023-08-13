f88b974d109a2cc0ded95a1bb26ed392f1fa2d9a
ankushpck <138556460+ankushpck@users.noreply.github.com>
2023-08-14 01:04:12 +0530

    avalancheproject3.sol

--- avalancheproject3.sol
+++ avalancheproject3.sol
@@ -1,1 +1,23 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CustomToken is ERC20, Ownable {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        require(to != address(0), "Transfer to the zero address");
+        _transfer(_msgSender(), to, amount);
+        return true;
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
